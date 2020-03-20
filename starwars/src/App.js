9c528904d1d92b6beea9c7e08fc71fa6b5102cf0
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-03-20 12:00:57 -0500

    done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
+import CharacterGrid from './components/CharacterGrid';
+import styled from 'styled-components';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+const Container = styled.div`
+  display: flex;
+  max-width:100%;
+  flex-wrap: wrap;
+  justify-content:space-evenly;
+  padding:40px;
+`;
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+      <Container>
+        <CharacterGrid />
+      </Container>
+    </div>   
   );
 }
 
